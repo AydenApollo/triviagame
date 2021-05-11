6afa506a94fef6d91ed3b5f48aabd74071da3f95
Shane McDonald <mcdonaldshane33@gmail.com>
2021-05-10 22:28:44 -0500

    minor updates, still really confused

--- jgame.js
+++ jgame.js
@@ -1,43 +1,16 @@
-const playerOne = document.getElementById("player-one");
-const playerTwo = document.getElementById("player-two");
-const currentPlayer = playerOne
+const beginButton = document.getElementById('startgame');
+const playerOne = document.getElementById('playerone');
+const playerTwo = document.getElementById('playertwo');
 
-
-
-
-var api_url = 'https://opentdb.com/api.php?amount=5&type=multiple'
-var config = {
-    parameters: {
-        category: "Entertainment",
-        questions: null,
-        answers: "correct_answer"
-    }
-};
-axios.get(api_url, config)
-.then(function (response) {
-    config.parameters.questions = response.data.results
-    console.log(config.parameters)
-    generateRandQuestion();
-})
-
-
-/*function generateRandQuestion(questions) {
-    const randIndex = Math.floor(Math.random() * config.parameters.questions.length + 1)
-    questions = config.parameters.questions
-    return (questions[randIndex])
-}*/
-
-
-class Player {
-    constructor (type) {
-        this.type = type
-        generateRandQuestion(questions) {
-            const randIndex = Math.floor(Math.random() * config.parameters.questions.length + 1)
-            questions = config.parameters.questions
-            return (questions[randIndex])
-        }
-    }
+const getApiData = () => {
+    axios.get('https://opentdb.com/api.php?amount=6&type=multiple')
+    .then(response => {
+        console.log(response)
+    })
+    .then(response => {
+        playerOne.innerHTML = "Player One's Turn!!"
+    })   
 }
 
-var playerQuestion = new Player('round')
-playerQuestion.generateRandQuestion()
+beginButton.addEventListener('click', getApiData)
+
