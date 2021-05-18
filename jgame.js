224c00c53a8a905e4d47a356ded9298881f6d36c
Shane McDonald <mcdonaldshane33@gmail.com>
2021-05-18 01:28:23 -0700

    tonight's updates

--- jgame.js
+++ jgame.js
@@ -1,45 +1,105 @@
+const category_api = document.getElementById('category');
+const difficulty_api = document.getElementById('difficulty');
+const question_api = document.getElementById('question');
+const ansBtn1 = document.getElementById('answer1')
+const ansBtn2 = document.getElementById('answer2')
+const ansBtn3 = document.getElementById('answer3')
+const ansBtn4 = document.getElementById('answer4')
+const questionCounter = document.getElementById('questionCounter')
+const currentPlayer = document.getElementById('playerTurn')
+const playerOne = "Player One"
+const playerTwo = "Player Two"
+
+
+
 class Game {
     constructor() {
         this.questions = [];
+        
     }
-    async fetchQuestions() {
-        let response = await axios.get('https://opentdb.com/api.php?amount=4&type=multiple');
-        console.log(response.data);
-        this.questions = response.data.results;
+      async fetchQuestions() {
+        await axios.get('https://opentdb.com/api.php?amount=4&type=multiple')
+       .then(response => {
+           console.log(response)
+           this.questions = response.data.results
+           console.log(this.questions.results)
+           
+
+       })
+    }
+    questionTracker() {
+        let questInt = localStorage.getItem('counter')
+        if (questInt == 0) {
+            localStorage.setItem('counter', 0);
+            //console.log(localStorage.getItem('counter'));
+            //questionCounter.innerHTML = "Question " + localStorage.getItem('counter') + " of 4"
+        }
+        console.log(localStorage.getItem('counter'))
+        questionCounter.innerHTML = "Question " + localStorage.getItem('counter') + " of 4"
     }
 
-    useData() {
-        document.querySelector('#category').innerHTML = `Category: ${this.questions[0].category}`;
-        document.querySelector('#difficulty').innerHTML = `Difficulty: ${this.questions[0].difficulty}`;
-        return;
-        document.querySelector('#question').innerHTML = `Question: ${data.results[0].question}`
-        document.querySelector('#answer1').innerHTML = `Option One: ${data.results[0].correct_answer}`
-        document.querySelector('#answer2').innerHTML = `Option Two: ${data.results[0].incorrect_answers[0]}`
-        document.querySelector('#answer3').innerHTML = `Option Three: ${data.results[0].incorrect_answers[1]}`
-        document.querySelector('#answer4').innerHTML = `Option Four: ${data.results[0].incorrect_answers[2]}`
-    }
+       useData() {
+           this.questions.map(data => {
+            category_api.innerHTML = `Category: ${data.category}` 
+            difficulty_api.innerHTML = `Difficulty: ${data.difficulty}`
+            question_api.innerHTML = `Question: ${data.question}`
+            ansBtn1.innerHTML = `${data.correct_answer}`
+            ansBtn2.innerHTML = `${data.incorrect_answers[0]}`
+            ansBtn3.innerHTML = `${data.incorrect_answers[1]}`
+            ansBtn4.innerHTML = `${data.incorrect_answers[2]}`
+           })
+       }
+       
+       
 }
 
-
-
-function useApiData(data) {
-    document.querySelector('#category').innerHTML = `Category: ${data.results[0].category}`
-    document.querySelector('#difficulty').innerHTML = `Difficulty: ${data.results[0].difficulty}`
-    document.querySelector('#question').innerHTML = `Question: ${data.results[0].question}`
-    document.querySelector('#answer1').innerHTML = `Option One: ${data.results[0].correct_answer}`
-    document.querySelector('#answer2').innerHTML = `Option Two: ${data.results[0].incorrect_answers[0]}`
-    document.querySelector('#answer3').innerHTML = `Option Three: ${data.results[0].incorrect_answers[1]}`
-    document.querySelector('#answer4').innerHTML = `Option Four: ${data.results[0].incorrect_answers[2]}`
-}
-
-
-var game;
-
-$('#answer1').click(async () => {
-    alert("Correct. Next Player's Turn")
-    //getApi()
-
-    game = new Game();
-    await game.fetchQuestions();
-    game.useData();
+/*function questionTracker(){
+    window.addEventListener("unload", function(){
+      var count = parseInt(localStorage.getItem('counter') || 0);
+      localStorage.setItem('counter', count++);
+      console.log(count)
+    }); 
+    
+  }*/
+  let currentPosition;
+$('#start').click(async () => {
+    //questionCounter.innerHTML = "Question 1 of 4"
+    let startGame = new Game();
+    await startGame.fetchQuestions();
+    startGame.useData()
+    currentPlayer.innerHTML = `${playerOne}`
+    startGame.questionTracker()    
 })
+$('#reload').click(async () => {
+    //questionCounter.innerHTML = "Question 1 of 4"
+    let startGame = new Game();
+    await startGame.fetchQuestions();
+    startGame.useData()
+    currentPlayer.innerHTML = `${playerTwo}`
+    
+})
+$('#answer1').click(async () => {
+    let startGame = new Game();
+    await startGame.fetchQuestions();
+    startGame.useData() 
+    startGame.questionTracker()
+    if (currentPosition) {
+        localStorage.setItem('counter', parseInt(localStorage.getItem('counter')) + 1)
+    }
+})
+$('#answer2').click(async () => {
+    let startGame = new Game();
+    await startGame.fetchQuestions();
+    startGame.useData() 
+})
+$('#answer3').click(async () => {
+    let startGame = new Game();
+    await startGame.fetchQuestions();
+    startGame.useData() 
+})
+$('#answer4').click(async () => {
+    let startGame = new Game();
+    await startGame.fetchQuestions();
+    startGame.useData() 
+})
+
